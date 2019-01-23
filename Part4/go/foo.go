fdadfe6bade8a8b2c0efba77bbffbc8b37913091
Peter Bull Hove <peterbullhove@gmail.com>
2019-01-23 11:57:52 +0100

    Test fra atom

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -3,37 +3,37 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	"time"
 )
 
 var i = 0
 
 func incrementing() {
-    //TODO: increment i 1000000 times
-    for k := 0 ; k < 1000000; k++ {
-        i+=1;
-    }
+	//TODO: increment i 1000000 times
+	for k := 0; k < 1000000; k++ {
+		i += 1
+	}
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
-    for k := 0 ; k < 1000000; k++ {
-        i-=1;
-    }
+	//TODO: decrement i 1000000 times
+	for k := 0; k < 1000000; k++ {
+		i -= 1
+	}
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
+	runtime.GOMAXPROCS(runtime.NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
+	// Try doing the exercise both with and without it!
 
-    // TODO: Spawn both functions as goroutines
-    go incrementing() 
-    go decrementing()
+	// TODO: Spawn both functions as goroutines
+	go incrementing()
+	go decrementing()
 
-    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
-    // We'll come back to using channels in Exercise 2. For now: Sleep.
-    time.Sleep(100*time.Millisecond)
-    Println("The magic number is:", i)
+	// We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+	// We'll come back to using channels in Exercise 2. For now: Sleep.
+	time.Sleep(100 * time.Millisecond)
+	Println("The magic number is:", i)
 }
